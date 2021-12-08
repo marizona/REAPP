5417cb50807c807af27d49babc4a0a387e449959
marizona <marie.jacquier@epitech.eu>
2021-12-08 12:50:49 +0100

    landing

--- client/src/Components/Landing.jsx
+++ client/src/Components/Landing.jsx
@@ -1,40 +1,65 @@
 import React from "react";
-import house from "../images/house.png";
+import house from "../images/house.jpg";
 
 export default function Landing() {
   return (
-    <div>
-      <div class="grid gap-4 grid-cols-2 mt-52 rounded-lg bg-gray-200 shadow-xl p-6">
-        
-        
-        <div className="box1">
+    <div className="max-w-screen-xl mt-52 px-8 xl:px-16 mx-auto bg-gray-100 shadow rounded-lg">
+      <div className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-8 py-6 sm:py-16 ">
+        <div className=" flex flex-col justify-center items-start row-start-2 sm:row-start-1">
           {" "}
-          <h1 className="text-5xl font-black  text-purple-700  font-sans">
-            Booking Done Right. Finally.
+          <h1 className=" text-6xl lg:text-3xl xl:text-5xl font-medium text-purple-900 leading-normal">
+            Booking Done Right.
           </h1>
-          <p className="text-lg">
+          <p className="text-black-500 mt-4 mb-6">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
             sapiente inventore non voluptatum cum, enim, veniam nesciunt
             cupiditate nisi voluptas necessitatibus hic culpa veritatis? Minus
             numquam totam sunt quos itaque?
           </p>
-          <div className="flex">
-            <button className="shadow-2xl bg-opacity-25  bg-black text-white py-3 px-10 text-xl rounded-t-lg focus:bg-white focus:text-gray-500">
-              Book
-            </button>
-            <button className="shadow-2xl bg-opacity-25  bg-black text-white py-3 px-10 text-xl rounded-t-lg focus:bg-white focus:text-gray-500">
-              Buy
-            </button>{" "}
-          </div>
-          <input
-            placeholder="Where do you want to live?"
-            className="py-6 placeholder:text-2xl shadow-2xl rounded hover: px-40"
-          ></input>
+          <button className="py-3 lg:py-4 px-12 lg:px-16 text-white-500 font-semibold rounded-lg bg-orange-500 hover:shadow-orange-md transition-all outline-none undefined"> Get Started</button>
         </div>
 
-
-        <div className="box2">
-          <img classname="w-96 h-96" src={house} alt="house" />
+        <div className="flex w-full">
+          <div className="h-full w-full">
+            <div
+              style={{
+                display: "block",
+                overflow: "hidden",
+                position: "relative",
+                boxSizing: "border-box",
+                margin: "0",
+              }}
+            >
+              <div
+                style={{
+                  display: "block",
+                  boxSizing: "border-box",
+                  paddingTop: "62.58169934640523%",
+                }}
+              ></div>
+              <img
+                className=" rounded-lg "
+                style={{
+                  visibility: "visible",
+                  position: "absolute",
+                  inset: "0px",
+                  boxSizing: "borderBox",
+                  padding: "0px",
+                  border: "medium none",
+                  margin: "auto",
+                  display: "block",
+                  width: "0px",
+                  height: "0px",
+                  minWidth: "100%",
+                  maxWidth: "100%",
+                  minHeight: "100%",
+                  maxHeight: "100%",
+                }}
+                src={house}
+                alt="house"
+              />
+            </div>
+          </div>
         </div>
       </div>
 
