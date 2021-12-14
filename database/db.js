bab9b1a0bbcf5ec162d7ec7fdba8d4329338e026
marizona <marie.jacquier@epitech.eu>
2021-12-14 11:04:42 +0100

    .env

--- database/db.js
+++ database/db.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
+
 
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://marizona:madura27@cluster0.tb1uv.mongodb.net/Rentify?retryWrites=true&w=majority",
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tb1uv.mongodb.net/Rentify?retryWrites=true&w=majority`,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
