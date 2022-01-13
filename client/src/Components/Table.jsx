61995d7e8696622d42eb79521a842b6605786f19
marizona <marie.jacquier@epitech.eu>
2022-01-13 13:03:40 +0100

    modal add

--- client/src/Components/Table.jsx
+++ client/src/Components/Table.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 
-
 import Loader from "./Loader";
 import {
   listProducts,
@@ -15,7 +14,6 @@ import { Link } from "react-router-dom";
 function Table({ history, match }) {
   const [modalOpen, setModalOpen] = useState(false);
 
-  
   const dispatch = useDispatch();
 
   const productList = useSelector((state) => state.productList);
@@ -66,7 +64,7 @@ function Table({ history, match }) {
         type="button"
         data-modal-toggle="authentication-modal"
       >
-        Toggle modal
+        + Add a product
       </button>
       {/* toogle */}
       <div
@@ -74,7 +72,7 @@ function Table({ history, match }) {
         aria-hidden="true"
         className="hidden overflow-y-auto overflow-x-hidden fixed right-0 left-0 top-4 z-50 justify-center items-center h-modal md:h-full md:inset-0"
       >
-        <div className="relative px-4 w-full max-w-lg h-full lg:h-auto">
+        <div className="relative px-4 w-full max-w-xl h-full lg:h-auto">
           {/* Modal content */}
           <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <div className="flex justify-end p-2">
@@ -102,59 +100,188 @@ function Table({ history, match }) {
               action="#"
             >
               <h3 className="text-xl font-medium text-gray-900 dark:text-white">
-                Sign in to our platform
+                Add a new Product
               </h3>
+              <div className="flex space-x-10">
+              <div>
               <div>
                 <label
-                  for="email"
+                  for="text"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                 >
-                  Your email
+                  title
                 </label>
                 <input
-                  type="email"
-                  name="email"
-                  id="email"
+                  type="text"
+                  name="title"
+                  id="title"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                  placeholder="name@company.com"
+                  placeholder="eg. house on Riverside Avenue"
                   required
                 />
               </div>
               <div>
                 <label
-                  for="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  for="user_avatar"
                 >
-                  Your password
+                  Upload images
                 </label>
                 <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="••••••••"
+                  class="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none focus:border-transparent dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                  aria-describedby="user_avatar_help"
+                  id="user_avatar"
+                  type="file"
+                />
+              </div>
+              <div>
+                <label
+                  for="text"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Owner
+                </label>
+                <input
+                  type="text"
+                  name="title"
+                  id="title"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                  placeholder="Name"
                   required
                 />
               </div>
-              <div className="flex justify-between">
-                <div className="flex items-start">
-                  <div className="flex items-center h-5">
-                    <input
-                      id="remember"
-                      aria-describedby="remember"
-                      type="checkbox"
-                      className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-                      required
-                    />
-                  </div>
-                </div>
+
+              <label
+                for="countries"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+              >
+                Select a Category
+              </label>
+              <select
+                id="countries"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              >
+                <option>House</option>
+                <option>Villa</option>
+                <option>Flat</option>
+                <option>TownHouse</option>
+              </select>
+
+              <label
+                for="message"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+              >
+                Description
+              </label>
+              <textarea
+                id="message"
+                rows="4"
+                class="block p-2 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Leave a description ..."
+              ></textarea>
               </div>
+<div>
+                <div>
+                <label
+                  for="text"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Location
+                </label>
+                <input
+                  type="text"
+                  name="title"
+                  id="title"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                  placeholder="City"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  for="text"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Surface
+                </label>
+                <input
+                  type="number"
+                  name="title"
+                  id="title"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                  placeholder="ft²"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  for="text"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Bathrooms
+                </label>
+                <input
+                  type="number"
+                  name="title"
+                  id="title"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                  placeholder="ft²"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  for="text"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Bedrooms
+                </label>
+                <input
+                  type="number"
+                  name="title"
+                  id="title"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                  placeholder="ft²"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  for="text"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Price
+                </label>
+                <input
+                  type="number"
+                  name="title"
+                  id="title"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                  placeholder="ft²"
+                  required
+                />
+              </div>
+              <label
+                for="countries"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+              >
+                Status
+              </label>
+              <select
+                id="countries"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              >
+                <option>Available</option>
+                <option>Sold</option>
+              </select>
               <button
                 type="submit"
                 className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 submit
               </button>
+              </div>
+              </div>
             </form>
           </div>
         </div>
@@ -216,34 +343,50 @@ function Table({ history, match }) {
                       </td>
                       <td className="px-6 py-4 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
                         {product.price}
-                        
                       </td>
                       <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                      <button onClick={() => {
-          setModalOpen(true)}} className="text-blue-600 hover:text-blue-900 dark:text-blue-500 dark:hover:underline" >Delete</button>
-                      {modalOpen && <div setOpenModal={setModalOpen}>
-                      
-                      <div onClick={() => {
-                        setModalOpen(false);
-                      }} class="bg-slate-800 bg-opacity-10  rounded-lg shadow bg-gray-700 flex justify-center items-center absolute top-0 right-0 bottom-0 left-0">
-                    <div class="bg-white px-16 py-14 shadow border-2 rounded-md text-center">
-                    <div className="titleCloseBtn">
-                         
-                        </div>
-                      <h1 class="text-xl font-bold text-slate-500">Do you Want Delete this product ?</h1>
-                      <p className='py-4'>This action cannot be undone</p>
-                      <button class="bg-red-500 px-4 py-2 rounded-md text-md text-white">Cancel</button>
-                      <button onClick={() => deleteHandler(product._id)} class=" btn btn-green px-7 py-2 ml-2 rounded-md text-md text-white font-semibold">Ok</button>
-                    </div>
-                  </div>
-                  </div>
-                      }
+                        <button
+                          onClick={() => {
+                            setModalOpen(true);
+                          }}
+                          className="text-blue-600 hover:text-blue-900 dark:text-blue-500 dark:hover:underline"
+                        >
+                          Delete
+                        </button>
+                        {modalOpen && (
+                          <div setOpenModal={setModalOpen}>
+                            <div
+                              onClick={() => {
+                                setModalOpen(false);
+                              }}
+                              class="bg-slate-800 bg-opacity-10  rounded-lg shadow bg-gray-700 flex justify-center items-center absolute top-0 right-0 bottom-0 left-0"
+                            >
+                              <div class="bg-white px-16 py-14 shadow border-2 rounded-md text-center">
+                                <div className="titleCloseBtn"></div>
+                                <h1 class="text-xl font-bold text-slate-500">
+                                  Do you Want Delete this product ?
+                                </h1>
+                                <p className="py-4">
+                                  This action cannot be undone
+                                </p>
+                                <button class="bg-red-500 px-4 py-2 rounded-md text-md text-white">
+                                  Cancel
+                                </button>
+                                <button
+                                  onClick={() => deleteHandler(product._id)}
+                                  class=" btn btn-green px-7 py-2 ml-2 rounded-md text-md text-white font-semibold"
+                                >
+                                  Ok
+                                </button>
+                              </div>
+                            </div>
+                          </div>
+                        )}
                       </td>
                     </tr>
                   ))}
                 </tbody>
               </table>
-    
             </div>
           </div>
         </div>
