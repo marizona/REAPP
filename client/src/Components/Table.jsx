7389639a28bdca063616fc29c2e086db694dc2fb
marizona <marie.jacquier@epitech.eu>
2022-01-13 11:36:22 +0100

    modal delete

--- client/src/Components/Table.jsx
+++ client/src/Components/Table.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
+
 
 import Loader from "./Loader";
 import {
@@ -9,8 +10,12 @@ import {
   createProduct,
 } from "../actions/productActions";
 import { PRODUCT_CREATE_RESET } from "../constants/productConstants";
+import { Link } from "react-router-dom";
 
 function Table({ history, match }) {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  
   const dispatch = useDispatch();
 
   const productList = useSelector((state) => state.productList);
@@ -37,7 +42,7 @@ function Table({ history, match }) {
     }
 
     if (successCreate) {
-      history.push(`/admin/product/${createdProduct._id}/edit`);
+      history.push(`/dashboard`);
     } else {
       dispatch(listProducts(""));
     }
@@ -57,7 +62,7 @@ function Table({ history, match }) {
   return (
     <div className="mx-auto lg:mt-12 md:mt-34 sm:mt-44">
       <button
-        className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="block text-white btn btn-green  font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         type="button"
         data-modal-toggle="authentication-modal"
       >
@@ -211,94 +216,34 @@ function Table({ history, match }) {
                       </td>
                       <td className="px-6 py-4 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
                         {product.price}
+                        
                       </td>
                       <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                        <button
-                          data-modal-toggle="popup-modal"
-                          className="block text-blue-600 hover:text-blue-900 dark:text-blue-500 dark:hover:underline"
-                        >
-                          Edit
-                        </button>
-                        {/* <!-- Modal toggle --> */}
-                        <button
-                          className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                          type="button"
-                          data-modal-toggle="popup-modal"
-                        >
-                          d
-                        </button>
-
-                        {/* <!-- Delete Product Modal --> */}
-                        <div
-                          className="hidden overflow-y-auto overflow-x-hidden fixed right-0 left-0 top-4 z-50 justify-center items-center md:inset-0 h-modal sm:h-full"
-                          id="popup-modal"
-                        >
-                          <div class="relative px-4 w-full max-w-md h-full md:h-auto">
-                            {/* <!-- Modal content --> */}
-                            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                              {/* <!-- Modal header --> */}
-                              <div class="flex justify-end p-2">
-                                <button
-                                  type="button"
-                                  className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
-                                  data-modal-toggle="popup-modal"
-                                >
-                                  <svg
-                                    className="w-5 h-5"
-                                    fill="currentColor"
-                                    viewBox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                  >
-                                    <path
-                                      fill-rule="evenodd"
-                                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                      clip-rule="evenodd"
-                                    ></path>
-                                  </svg>
-                                </button>
-                              </div>
-                              {/* <!-- Modal body --> */}
-                              <div class="p-6 pt-0 text-center">
-                                <svg
-                                  className="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200"
-                                  fill="none"
-                                  stroke="currentColor"
-                                  viewBox="0 0 24 24"
-                                  xmlns="http://www.w3.org/2000/svg"
-                                >
-                                  <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                                  ></path>
-                                </svg>
-                                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                                  Are you sure you want to delete this product?
-                                </h3>
-                                <button
-                                  data-modal-toggle="popup-modal"
-                                  type="button"
-                                  className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
-                                >
-                                  Yes, I'm sure
-                                </button>
-                                <button
-                                  data-modal-toggle="popup-modal"
-                                  type="button"
-                                  className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600"
-                                >
-                                  No, cancel
-                                </button>
-                              </div>
-                            </div>
-                          </div>
+                      <button onClick={() => {
+          setModalOpen(true)}} className="text-blue-600 hover:text-blue-900 dark:text-blue-500 dark:hover:underline" >Delete</button>
+                      {modalOpen && <div setOpenModal={setModalOpen}>
+                      
+                      <div onClick={() => {
+                        setModalOpen(false);
+                      }} class="bg-slate-800 bg-opacity-10  rounded-lg shadow bg-gray-700 flex justify-center items-center absolute top-0 right-0 bottom-0 left-0">
+                    <div class="bg-white px-16 py-14 shadow border-2 rounded-md text-center">
+                    <div className="titleCloseBtn">
+                         
                         </div>
+                      <h1 class="text-xl font-bold text-slate-500">Do you Want Delete this product ?</h1>
+                      <p className='py-4'>This action cannot be undone</p>
+                      <button class="bg-red-500 px-4 py-2 rounded-md text-md text-white">Cancel</button>
+                      <button onClick={() => deleteHandler(product._id)} class=" btn btn-green px-7 py-2 ml-2 rounded-md text-md text-white font-semibold">Ok</button>
+                    </div>
+                  </div>
+                  </div>
+                      }
                       </td>
                     </tr>
                   ))}
                 </tbody>
               </table>
+    
             </div>
           </div>
         </div>
